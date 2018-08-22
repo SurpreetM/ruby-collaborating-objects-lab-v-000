6c7711c08df846f3322c334ba68cb3620064779b
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-22 15:53:45 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,8 +9,12 @@ class MP3Importer
   def files
     @files = Dir.glob('#{path}/*.mp3').collect do |file|
       f.gsub("#{path}/", "")
-    end 
+    end
+  end
 
+  def import(files)
+    files.each do |file| Song.new_by_filename(file)
+    end 
   end
 
 end
