7cb735bb68e86efaf12b9b9b32bd57612f50e140
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-22 15:51:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,16 @@
 class Song
 
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(filename)
+    song = self.new
+    song.artist = filename.split(" - ")[0]
+    song.name = filename.split(" - ")[1]
+    song
+  end
+
 end
