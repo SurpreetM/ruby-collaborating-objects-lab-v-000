2e4698fa0ea2a72885796fa3f8e6712c4b1a74bf
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-22 16:30:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
 
   def self.new_by_filename(filename)
     song = self.new(filename.split(" - ")[1])
-    song.artist.name = filename.split(" - ")[0]
+    song.artist_name = filename.split(" - ")[0]
     song
   end
 
