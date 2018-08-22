aa5e28b98a50af4d64e6492011f5187c4b7176b7
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-22 17:06:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,20 +5,23 @@ class Artist
 
   def initialize(name)
     @name = name
-    @songs = []
   end
 
-  def add_song(song)
-    @songs << song
+  def songs
+    Song.find_by_artist(self)
   end
 
   def self.find_or_create_by_name(name)
     self.find(name) ?
-    self.find(name) : self.new(name)
+    self.find(name) : self.create(name)
   end
 
   def self.find(name)
    self.all.find {|artist| artist.name == name }
+  end
+
+  def self.create(name)
+   self.new(name).tap {|artist| artist.save}
   end
 
   def self.all
